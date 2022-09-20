af9462b1cc1d16f7816c0e6280baa3960c7be529
Denta Hidayat <denta.dent53@gmail.com>
2022-09-20 14:19:49 +0700

    addingrequest animation frame

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ function main() {
 
   */
 
-  var vertices = [0.5, 0.5, 0.0, 0.0, -0.5, 0.5, 0.0, 0.8]; // VERTICES
+  var vertices = [
+    0.5, 0.5, 
+    0.0, 0.0, 
+    -0.5, 0.5, 
+    0.0, 1.0
+  ]; // VERTICES
 
   // Create a linked-list for storing the vertices data in GPU realm
   var buffrer = gl.createBuffer();
@@ -23,38 +28,29 @@ function main() {
   attribute vec2 aPosition;
   uniform float uTheta;
   void main () {
-    gl_PointSize = 15.0;  // adding size of point
-    vec2 position = vec2(aPosition)
-    position.x = -sin(uTheta) * position.x + cos(uTheta) * position.y;
-    position.y = sin(uTheta) * position.x + cos(uTheta) * position.y;
-    gl_Position = vec4(aPosition, 0.0, 1.0);
-    // is the final destination for storing
-    // positional data for the rendered vertex
+    gl_PointSize = 15.0;
+    vec2 position = vec2(aPosition);
+    position.x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+    position.y = sin(uTheta) * aPosition.y + cos(uTheta) * aPosition.x;
+    gl_Position = vec4(position, 0.0, 1.0);
   }
   `;
 
-  // Create shader to vertext shader
+
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderCode); // passing Shader from CPU to GPU
+  gl.shaderSource(vertexShader, vertexShaderCode); 
   gl.compileShader(vertexShader);
 
-  // FRAGMENT SHADER
   var fragmentShaderCode = `
       precision mediump float; // useful practice
 
         void main () {
-          // final color for (they need to render in particular fragment) fev1/fec2 dll is for vector
-          // final destinarion for storing 
           gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
         }
   `;
   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShader, fragmentShaderCode);
   gl.compileShader(fragmentShader);
-
-  // Comparing to C programming, we my imagine
-  // that up to this step we have created two
-  // object files (.o) for the vertex and fragment shader
 
   var shaderProgram = gl.createProgram();
   gl.attachShader(shaderProgram, vertexShader); // After the shader program has been created
@@ -65,33 +61,20 @@ function main() {
 
   var theta = 0.0;
   // ! all qualifire
-  var uTheta = gl.getUniformLocation(shaderProgram, "UTheta");
-
-  // Teach the GPU how to collect the potitional values from ARRAY_BUFFER
-  // for each vertex being processed
+  var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
   var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
   gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(aPosition);
 
-
-
-  function render() {
+function render() {
   gl.clearColor(1.0, 0.75, 0.79, 1.0); // adding a color background
-
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  theta += 0.1;
+  
+  theta += 0.01;
   gl.uniform1f(uTheta, theta);
 
-  // draw the canvas using drawArrays
-  // glPOINTS (Assembly)
-  gl.drawArrays(gl.TRIANGLES_FAN, 0, 4);
-  // LINES = only 1 line
-  // LINE_LOOP = loop to the first coordinate
-  // LINE_STRIP = not looping to the first loop
-  // TRIANGLES =  draw tiriangles
-  // TRIANGLES_STRIP, TRIANGLES_LOOP etc
-  // TRIANGLES_FAN = draw a fan
-  }
-  setInterval(render, 1000/60);
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
 }
